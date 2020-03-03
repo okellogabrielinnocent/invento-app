e8bf0ac661abd0678c6fe661d515cdc8dd4c3a90
Rockerfeller <okellogabrielinnocent@gmail.com>
2020-03-03 12:16:13 +0300

    Scarfold user auth (#1)

---
+++ app/Http/Middleware/CustomerMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CustomerMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(auth()->user()->isCustomer()) {
+            return $next($request);
+        }
+        return back()->withErrors(['permission_denied' => 'Not allowed to perform action']);
+    }
+}
