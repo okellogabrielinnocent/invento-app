fcb1071113482d26cbc869033185b612d3a4552f
Gabriel Innocent Okello <Gabriel@users-MacBook-Pro.local>
2020-03-19 16:15:22 +0300

    create service sale

---
+++ database/factories/ServiceSaleFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ServiceSale;
+use Faker\Generator as Faker;
+
+$factory->define(ServiceSale::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
